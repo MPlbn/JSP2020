54b9f677d84c3fbb7ecbd27fdaf782693fc4c317
MPlbn <mi8ki@go2.pl>
2020-12-10 09:15:31 +0100

    Update Zadanie 2.py

--- Lista7/Zadanie 2.py
+++ Lista7/Zadanie 2.py
@@ -1,19 +1,21 @@
 import random
 
 def sortowanie(x):
-    for i in range(len(x) - 1):
+    for i in range(len(x)):
         key = x[i]
         j = i-1
         while ((j >= 0) and (x[j] > key)):
             x[i] = x[j]
             x[j] = key
             j -= 1
-    return x  
+            i -= 1
+    return x
 
 list1 = [random.randint(1,20) for _ in range(100)]
 list2 = [random.randint(1,20) for _ in range(200)]
 list3 = [random.randint(1,20) for _ in range(300)]
 
-lista = [3,7,1,4,19,2]
 
-print(sortowanie(lista))
+print(sortowanie(list1))
+print(sortowanie(list2))
+print(sortowanie(list3))

707ffa3e313f56c73d7ea9515b9e74eed41859f5
MPlbn <mi8ki@go2.pl>
2021-01-28 10:39:49 +0100

    Update Zadanie 3.py

--- Lista9/Zadanie 3.py
+++ Lista9/Zadanie 3.py
@@ -2,16 +2,29 @@ import numpy as n
 import matplotlib.pyplot as mplot
 wall = 1
 
-t = n.arange(0,wall,0.00001)
+t = n.arange(0,wall,0.001)
 g = 9.81
-deg = 30 #int(input('Podaj wartosc kata (1 - 90): '))
+deg = int(input('Podaj wartosc kata (1 - 90): '))
 rad = deg/180 * n.pi
 
-v0 = 10 #int(input('Podaj wartosc predkosci poczatkowej: '))
-vx = v0 * n.cos(rad) 
-vy0 = v0 * n.sin(rad)
+v0 = int(input('Podaj wartosc predkosci poczatkowej: '))
+vx = v0 * n.cos(rad)
+vxl = []
+for i in list(range(0, 1000)):
+    vxl.append(vx) 
+    vy0 = v0 * n.sin(rad)
 vy = v0 * n.sin(rad) - (g * t)
-
+x = vx * t
 y = v0 * t * n.sin(rad) - (g/2) * (t ** 2)
 ymax = n.max(y) #Maksymalna wysokosc
-n.roots(y)
+
+
+mplot.subplot(3,1,1)
+mplot.plot(t,vy,t,vxl)
+mplot.title('wykres prędkosci w czasie')
+mplot.subplot(3,1,2)
+mplot.plot(t,y)
+mplot.title('wykres położenia w czasie')
+mplot.subplot(3,1,3)
+mplot.plot(x,y)
+mplot.title('wykres toru rzutu')
